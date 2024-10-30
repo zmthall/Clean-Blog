6e2c50156f3dd9c8b8725cff35ceb2f4ea801fc5
Zack Thallas <zmthall@ZMT-MacBook-Pro.local>
2024-10-29 19:54:16 -0600

    Added post entity

---
+++ Blog Backend/entities/post.js
@@ -0,0 +1,20 @@
+class post {
+    constructor({title, author, body, tags = []}) {
+        if(typeof title === 'string' && title.length <= 200) {
+            this.title = title;
+        }
+
+        if(typeof author === 'string' && author.length <= 100) {
+            this.author = author;
+            this.creator = author;
+        }
+
+        if(typeof body === 'string' && body.length <= 10000) {
+            this.body = body;
+        }
+
+        if(Array.isArray(tags) && tags.every(tag => typeof tag === 'string' && tag.length <= 10)) {
+            this.tags = tags;
+        }
+    }
+}
