0ee5d835f9b5d837213d0ff353a38a9cd1d96082
zmthall <zmthall@gmail.com>
2024-11-01 10:40:13 -0600

    Added comments and small changes

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,5 @@
 // Console logger for api requests
 export function logger(req, res, next) {
-    console.log(`[${new Date().toISOString()}] | ${req.method} | ${req.url} | ${req.headers['user-agent']}`);
+    console.log(`[${new Date().toISOString()}] ${req.method} | ${req.url} | ${req.headers['user-agent']}`);
     next();
 }
